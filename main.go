92f0952a3187a18537e2ccea071f0de255100037
Jonathan Wills <runningwild@gmail.com>
2013-09-19 21:08:56 -0700

    Lan discovery stuff

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/runningwild/cgf/core"
 	"log"
 	"net"
+	"time"
 )
 
 type Game interface {
@@ -50,6 +51,86 @@ func (e *Engine) Ids() []int64 {
 
 func (e *Engine) Kill() {
 	e.server.Kill()
+}
+
+func (e *Engine) Host() {
+}
+
+type HostData struct {
+	Ip   string
+	Name string
+}
+
+func Host(port int, name string) error {
+	addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return err
+	}
+	listen, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		return err
+	}
+	// TODO: Make this not have to run forever
+	go func() {
+		for {
+			msg := make([]byte, 1024)
+			_, remoteAddr, err := listen.ReadFromUDP(msg)
+			if err != nil {
+				return
+			}
+
+			// Take the ping we got and send back a pong so they know we're here
+			go func() {
+				conn, err := net.Dial("udp", remoteAddr.String())
+				if err != nil {
+					return
+				}
+				_, err = conn.Write([]byte("pong"))
+				if err != nil {
+					return
+				}
+				conn.Close()
+			}()
+		}
+	}()
+	return nil
+}
+
+// This will broadcast on LAN, wait for waitMS milliseconds, and return data for
+// each host on the LAN
+func SearchLANForHosts(port int, waitMs int) ([]HostData, error) {
+	// We set up the listener before pinging because we don't want someone to
+	// respond before we're listening.
+	addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+	listen, err := net.ListenUDP("udp", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	conn, err := net.Dial("udp", fmt.Sprintf("255.255.255.255:%d", port))
+	if err != nil {
+		return nil, err
+	}
+	_, err = conn.Write([]byte("ping"))
+	if err != nil {
+		return nil, err
+	}
+
+	listen.SetDeadline(time.Now().Add(time.Millisecond * time.Duration(waitMs)))
+	var hosts []HostData
+	for {
+		msg := make([]byte, 1024)
+		n, remoteAddr, err := listen.ReadFromUDP(msg)
+		if err != nil {
+			break
+		}
+		hosts = append(hosts, HostData{Ip: remoteAddr.String(), Name: string(msg[0:n])})
+	}
+
+	return hosts, nil
 }
 
 func NewHostEngine(game Game, frame_ms int, ip string, port int, logger *log.Logger) (*Engine, error) {
